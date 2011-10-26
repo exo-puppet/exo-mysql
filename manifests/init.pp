ec21e718242fc7a366dd35c0501e9934f27a6190
Arnaud Héritier <aheritier@apache.org>
2011-10-26 18:33:22 +0200

    Customize mysql plugin to allow to deploy specific configuration files

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,22 +2,48 @@
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
 # See LICENSE for the full license granted to you.
 
-class mysql::server {
+class mysql::server ($mysql_profile="") {
 
 	package { "mysql-server":
 		ensure => installed,
 	}
 
-	munin::plugin {
-		[mysql_bytes, mysql_queries, mysql_slowqueries, mysql_threads]:
-			config => "env.mysqlopts --defaults-file=/etc/mysql/debian.cnf\nuser root"
+ 	file {
+		"/home/mysql" :
+			ensure => directory,
+			require => Package["mysql-server"],
+			owner => "mysql",
+			group => "mysql";
 	}
+				
+	if ( $mysql_profile != "" ) {		
+	    file {
+	        "/etc/mysql/conf.d/${mysql_profile}.cnf":
+	            owner   => root,
+	            group   => root,
+	            mode    => 440,
+	            source  => "puppet:///modules/mysql/${mysql_profile}.cnf",
+	            require => Package["mysql-server"],
+	            ensure  => file,
+	    }
+    }
 
 	service { mysql:
 		ensure => running,
 		hasstatus => true,
 		require => Package["mysql-server"],
+		subscribe  => [ Package["mysql-server"], File["/etc/mysql/conf.d/${mysql_profile}.cnf"] ],
 	}
+
+    case $operatingsystem {
+      'ubuntu':  { 
+		    # mysql defaults
+		    Mysql_database { defaults => "/etc/mysql/debian.cnf" }
+		    Mysql_user { defaults => "/etc/mysql/debian.cnf" }
+		    Mysql_grant { defaults => "/etc/mysql/debian.cnf" }	
+      } # apply the debian class
+      default:  { } # apply the generic class
+    }
 
 	# Collect all databases and users
 	Mysql_database<<||>>
