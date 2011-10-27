fc06c46572e00326e01850da030bfde527c30a31
Arnaud Héritier <aheritier@apache.org>
2011-10-27 03:23:52 +0200

    Improve mysql setup for ubuntu. Allow to have a tuning profile with few parameters. Takes care of app armor.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,39 +2,66 @@
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
 # See LICENSE for the full license granted to you.
 
-class mysql::server ($mysql_profile="") {
+class mysql::server ($mysql_tunning="false",$mysql_max_connections=100,$mysql_innodb_buffer_pool_size="2G",$mysql_data_dir="") {
 
 	package { "mysql-server":
 		ensure => installed,
 	}
-
- 	file {
-		"/home/mysql" :
-			ensure => directory,
-			require => Package["mysql-server"],
-			owner => "mysql",
-			group => "mysql";
-	}
- 	file {
-		"/home/mysql/mysql" :
-			ensure => directory,
-			recurse => true,
-			replace => false,
-			require => File["/home/mysql"],
-			owner => "mysql",
-			group => "mysql",
-			source => "/var/lib/mysql/mysql";
-	}
 				
-	if ( $mysql_profile != "" ) {		
+	# Use another data directory			
+	if ( $mysql_data_dir != "" ) {
+		# Copy the mysql system database
+	 	file {
+			"$mysql_data_dir/mysql" :
+				ensure => directory,
+				recurse => true,
+				owner => "mysql",
+				group => "mysql",
+				source => "/var/lib/mysql/mysql",
+				require => Package["mysql-server"];
+		}
+		# Configure mysql to use the new data directory
 	    file {
-	        "/etc/mysql/conf.d/${mysql_profile}.cnf":
+	        "/etc/mysql/conf.d/relocation.cnf":
+	            ensure  => file,
 	            owner   => root,
 	            group   => root,
 	            mode    => 440,
-	            source  => "puppet:///modules/mysql/${mysql_profile}.cnf",
+	            content => template("${module_name}/relocation.cnf.erb"),
 	            require => Package["mysql-server"],
+	            notify => Service[mysql]
+	    }
+	    # On ubuntu update apparmor
+        case $operatingsystem {
+	      'ubuntu':  { 
+			service { apparmor:
+				ensure => running,
+				hasstatus => true,
+			}
+		    file {
+		        "/etc/apparmor.d/usr.sbin.mysqld":
+		            ensure  => file,
+		            owner   => root,
+		            group   => root,
+		            mode    => 644,
+		            content => template("${module_name}/usr.sbin.mysqld.erb"),
+		            require => Package["mysql-server"],
+		            notify => Service["mysql","apparmor"]
+			    }
+		      }
+		      default:  { } # apply the generic class
+		    }
+		}
+	if ( $mysql_tunning == "true" ) {		
+	    file {
+	        "/etc/mysql/conf.d/tunning.cnf":
 	            ensure  => file,
+	            owner   => root,
+	            group   => root,
+	            mode    => 440,
+	            content => template("${module_name}/tunning.cnf.erb"),
+	            require => Package["mysql-server"],
+	            notify => Service[mysql]
 	    }
     }
 
@@ -42,7 +69,7 @@ class mysql::server ($mysql_profile="") {
 		ensure => running,
 		hasstatus => true,
 		require => Package["mysql-server"],
-		subscribe  => [ Package["mysql-server"], File["/etc/mysql/conf.d/${mysql_profile}.cnf"], File["/home/mysql/mysql"] ],
+		subscribe  => [ Package["mysql-server"] ],
 	}
 
     case $operatingsystem {
