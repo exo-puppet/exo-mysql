58a6b25367b44d40bd69ba86afae3df54f3a5186
Frederic DROUET <fdrouet@exoplatform.com>
2012-01-16 16:45:09 +0100

    fix logs in mysql_root_password fact (Facter.info … work with Facter -p but not with puppet agent --test ;-()

--- lib/facter/mysql_root_password.rb
+++ lib/facter/mysql_root_password.rb
@@ -7,11 +7,9 @@ Facter.add( "mysql_root_password" ) do
     if File.exist? "/root/.my.cnf"
       existing_password = Facter::Util::Resolution.exec("cat  /root/.my.cnf | grep -m 1 password | sed 's/ //g' | cut -d= -f2").chomp
         #puts ["Fact (mysql_root_password) : existing password = '", existing_password, "'"].join
-        Facter.info(["Fact (mysql_root_password) : existing password = '", existing_password, "'"].join)
       existing_password
     else
       #puts ["Fact (mysql_root_password) : no existing password stored in /root/.my.cnf (File doesn't exists)."  ].join
-      Facter.warn( ["Fact (mysql_root_password) : no existing password stored in /root/.my.cnf (File doesn't exists)."  ].join )
       new_password = "false"
     end
   end
--- lib/puppet/parser/functions/mysql_random_password.rb
+++ lib/puppet/parser/functions/mysql_random_password.rb
@@ -5,7 +5,6 @@ module Puppet::Parser::Functions
     password_characters =  [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten
     new_password  =  Range.new(0,16).map{ password_characters[rand(password_characters.length)] }.join
     #puts [ "Function (mysql_random_password) : a new mysql password was generated : ", new_password ].join
-    Facter.info( [ "Function (mysql_random_password) : a new mysql password was generated : ", new_password ].join )
     new_password
   end
 end
