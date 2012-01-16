cba716e1e941825a172d5f301239e6a6d234bfc0
Frederic DROUET <fdrouet@exoplatform.com>
2012-01-16 16:34:16 +0100

    the mysql root password is no longer printed in the logs ;-)

--- lib/puppet/parser/functions/mysql_random_password.rb
+++ lib/puppet/parser/functions/mysql_random_password.rb
@@ -4,7 +4,8 @@ module Puppet::Parser::Functions
   newfunction(:mysql_random_password, :type => :rvalue) do |args|
     password_characters =  [('a'..'z'),('A'..'Z'),(0..9)].map{|i| i.to_a}.flatten
     new_password  =  Range.new(0,16).map{ password_characters[rand(password_characters.length)] }.join
-    puts [ "Function (mysql_random_password) : a new mysql password was generated : ", new_password ].join()
+    #puts [ "Function (mysql_random_password) : a new mysql password was generated : ", new_password ].join
+    Facter.info( [ "Function (mysql_random_password) : a new mysql password was generated : ", new_password ].join )
     new_password
   end
 end
